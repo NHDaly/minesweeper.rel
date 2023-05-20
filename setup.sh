d458b28107591ecf6481fbce062e8a3994f6337f
NHDaly <nathan.daly@relational.ai>
2023-05-20 09:17:09 -0600

    Update rel logic from rel-playground version
    - Simplify the display, and simultaneously work around bug.
    - comments in mines.rel
    - simplify init.rel back to a single step to reset
    - Add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,40 @@
+# setup.sh database engine
+# This setup file uses the RAI CLI to set up the minesweeper database
+
+DATABASE="$1"
+ENGINE="$2"
+
+echo "database: $DATABASE"
+echo "engine: $ENGINE"
+
+set -x  # print commands before running
+set -e  # exit on error
+
+# Delete the DB if it already exists
+rai delete-database "$DATABASE" -q
+rai create-database "$DATABASE"
+# Don't error if the engine already exists
+rai create-engine "$ENGINE" --size="S" -q  || echo "Engine already exists"
+
+# Dummy warmup
+rai exec "$DATABASE" -e "$ENGINE" -c '2+2+2'
+
+# Load the models from the position of this file
+(
+    cd $(dirname "$0");
+    rai load-models "$DATABASE" -e "$ENGINE" ./model/mines.rel ./model/display.rel ./model/init.rel
+)
+
+# Workaround for FD bug: Clear out the inserted tests and flags
+rai exec "$DATABASE" -e "$ENGINE" -c 'def insert:reset = true'
+
+# Warm up the display relation
+echo "First run: warmup........."
+rai exec "$DATABASE" -e "$ENGINE" -c 'display'
+
+# Warm up some transactions
+rai exec "$DATABASE" -e "$ENGINE" -c 'def insert:test = ^Coord[5, 5]'
+rai exec "$DATABASE" -e "$ENGINE" -c 'display'
+rai exec "$DATABASE" -e "$ENGINE" -c 'def insert:reset = true'
+rai exec "$DATABASE" -e "$ENGINE" -c 'display'
+
